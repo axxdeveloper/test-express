17e1c7b1e70e6a9b22f510910972fbbe4232bddd
shooeugenesea <shooeugenesea.tw@gmail.com>
2020-07-29 01:47:37 +0800

    Use express.static

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
 var express = require('express');
-var serveStatic =  require('serve-static');
-var app = express().use(  serveStatic(__dirname + '/public', {'index': ['lalala.html']})  ).listen(3000);
+var app = express().use(  express.static(__dirname + '/public', {'index': ['lalala.html']})  ).listen(3000);
