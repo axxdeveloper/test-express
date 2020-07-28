00d837d44707ab533d070ea1724614ea1c15a55b
shooeugenesea <shooeugenesea.tw@gmail.com>
2020-07-29 02:06:32 +0800

    Support serveIndex file list

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 var express = require('express');
-var app = express().use(  express.static(__dirname + '/public', {'index': ['lalala.html']})  ).listen(3000);
+var serveIndex = require('serve-index')
+var app = express()
+    .use( express.static(__dirname + '/public') )
+    .use( serveIndex(__dirname + '/public', {}) )
+    .listen(3000);
